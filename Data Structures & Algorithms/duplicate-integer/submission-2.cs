0cdad3fef033110e85538c8c1ada60d60a58e645
CodePlayG <55430676+CodePlayG@users.noreply.github.com>
2026-09-01 18:19:34 +0530

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public bool hasDuplicate(int[] nums) {
+        if(nums.Length<=1) return false;
+       HashSet<int> items = new();
+       foreach(int n in nums){
+        if(!(items.Add(n)))
+            return true;
+       }            
+         return false;
+               
+    }
+}
